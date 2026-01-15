3213440cd7202051f58cb0fcf320059be9aa2de7
ʀᴏᴏᴛᴢᴇʀᴏ_x <rootzero.xz@gmail.com>
2026-01-15 14:14:41 +0500

    Cors Error fixed

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/tapi": {
-        target: "https://694fc8f1e1918.myxvest1.ru",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/tapi/, ""),
-      },
-    },
-  },
 });
